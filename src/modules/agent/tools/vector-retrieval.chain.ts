8a8b52913c3ad835eebce5dd4aa8f4b8d3e05ec7
Raphael Ferreira Bertoldo <rxphxel7k@gmail.com>
2024-07-22 19:56:02 -0300

    ✅ Challenge - Creating a Retrieval Chain

--- src/modules/agent/tools/vector-retrieval.chain.ts
+++ src/modules/agent/tools/vector-retrieval.chain.ts
@@ -37,13 +37,43 @@ export default async function initVectorRetrievalChain(
   llm: BaseLanguageModel,
   embeddings: Embeddings
 ): Promise<Runnable<AgentToolInput, string>> {
-  // TODO: Create vector store instance
-  // const vectorStore = ...
-  // TODO: Initialize a retriever wrapper around the vector store
-  // const vectorStoreRetriever = ...
-  // TODO: Initialize Answer chain
-  // const answerChain = ...
-  // TODO: Return chain
-  // return RunnablePassthrough.assign( ... )
+  //  Create vector store instance
+  const vectorStore = await initVectorStore(embeddings);
+
+  // Initialize a retriever wrapper around the vector store
+  const vectorStoreRetriever = vectorStore.asRetriever(5);
+
+  // Initialize  Answer Chain
+  const answerChain = initGenerateAnswerChain(llm);
+
+  // Get the rephrased question and generate context
+  return RunnablePassthrough.assign({
+    documents: new RunnablePick("rephrasedQuestion").pipe(vectorStoreRetriever),
+  })
+    .assign({
+      // Extract the IDs
+      ids: new RunnablePick("documents").pipe(extractDocumentIds),
+      // convert documents to string
+      context: new RunnablePick("documents").pipe(docsToJson),
+    })
+    .assign({
+      output: (input: RetrievalChainThroughput) =>
+        answerChain.invoke({
+          question: input.rephrasedQuestion,
+          context: input.context,
+        }),
+    })
+    .assign({
+      responseId: async (input: RetrievalChainThroughput, options) =>
+        saveHistory(
+          options?.config.configurable.sessionId,
+          "vector",
+          input.input,
+          input.rephrasedQuestion,
+          input.output,
+          input.ids
+        ),
+    })
+    .pick("output");
 }
 // end::function[]
