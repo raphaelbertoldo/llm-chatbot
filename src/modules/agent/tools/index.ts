349379750f457e524d4a749fd388e2b4bc474b37
Raphael Ferreira Bertoldo <rxphxel7k@gmail.com>
2024-07-26 20:35:06 -0300

    ✅ Challeng - Agent Tools

--- src/modules/agent/tools/index.ts
+++ src/modules/agent/tools/index.ts
@@ -14,10 +14,32 @@ export default async function initTools(
   graph: Neo4jGraph
 ): Promise<DynamicStructuredTool[]> {
   // TODO: Initiate chains
+  const cypherChain = await initCypherRetrievalChain(llm, graph);
   // const cypherChain = await ...
   // const retrievalChain = await ...
+  const retrievalChain = await initVectorRetrievalChain(llm, embeddings);
 
   // TODO: Append chains to output
-  return [];
+  return [
+    // tag::cypher[]
+    new DynamicStructuredTool({
+      name: "graph-cypher-retrieval-chain",
+      description:
+        "For retrieving movie information from the database including movie recommendations, actors and user ratings",
+      schema: AgentToolInputSchema,
+      func: (input, _runManager, config) => cypherChain.invoke(input, config),
+    }),
+    // end::cypher[]
+    // tag::vector[]
+    new DynamicStructuredTool({
+      name: "graph-vector-retrieval-chain",
+      description:
+        "For finding movies, comparing movies by their plot or recommending a movie based on a theme",
+      schema: AgentToolInputSchema,
+      func: (input, _runManager: any, config) =>
+        retrievalChain.invoke(input, config),
+    }),
+    // end::vector[]
+  ];
 }
 // end::function[]
