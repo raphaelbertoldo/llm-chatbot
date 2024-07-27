f4364bad0913e7dee26e2f07663852d98d3fdac0
Raphael Ferreira Bertoldo <rxphxel7k@gmail.com>
2024-07-27 01:28:01 -0300

    🎉 Finish Callenge

--- src/modules/agent/index.ts
+++ src/modules/agent/index.ts
@@ -1,3 +1,4 @@
+import { embeddings } from "./../llm";
 import { ChatOpenAI } from "@langchain/openai";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import initAgent from "./agent";
@@ -7,7 +8,24 @@ import { sleep } from "@/utils";
 // tag::call[]
 export async function call(input: string, sessionId: string): Promise<string> {
   // TODO: Replace this code with an agent
-  await sleep(2000);
-  return input;
+  const llm = new ChatOpenAI({
+    configuration: {
+      baseURL: process.env.OPENAI_API_BASE,
+    },
+  });
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    configuration: {
+      baseURL: process.env.OPENAI_API_BASE,
+    },
+  });
+  const graph = await initGraph();
+  // end::graph[]
+
+  // tag::call[]
+  const agent = await initAgent(llm, embeddings, graph);
+  const res = await agent.invoke({ input }, { configurable: { sessionId } });
+
+  return res;
 }
 // end::call[]
